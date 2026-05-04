20b1aec5d3541f0e702de8af83ae468929b5f25f
silvyafr <afriliasilvy@gmail.com>
2026-05-04 19:32:48 +0700

    Add makanan array with food items and descriptions

---
+++ data.js
@@ -0,0 +1,17 @@
+const makanan = [
+  {
+    nama: "Asinan Bogor",
+    deskripsi: "Jajanan segar dengan rasa asam, manis, dan pedas.",
+    gambar: "https://id.pinterest.com/pin/838232549390041002/"
+  },
+  {
+    nama: "Toge Goreng",
+    deskripsi: "Toge, mie, dan lontong dengan bumbu tauco khas.",
+    gambar: "https://id.pinterest.com/pin/651544271104576416/"
+  },
+  {
+    nama: "Roti Unyil",
+    deskripsi: "Roti kecil dengan berbagai isian lezat.",
+    gambar: "https://id.pinterest.com/pin/576531189790559068/"
+  }
+];
