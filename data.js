2571c88e7d7623c93509c326ef331cbe3acbf431
Dini Husaini <dinihusaini2312@gmail.com>
2026-06-05 14:25:22 +0700

    Update data.js

--- data.js
+++ data.js
@@ -54,7 +54,7 @@ const dataKuliner = {
       deskripsi: "Minuman rempah tradisional.",
       lokasi: "Sekitar Bogor",
       harga: "Rp10.000",
-      gambar: "assets/birkotjok.jpeg",
+      gambar: "assets/bir-pletok-bogor.jpg",
       kategori: "rekomendasi"
     }
   ]
