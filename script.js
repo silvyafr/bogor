f6b0edd1333064ec1f382760d6e0797b5b0985fe
Dini Husaini <dinihusaini2312@gmail.com>
2026-07-04 11:01:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,35 +16,61 @@ const viralContainer = document.getElementById("viral-container");
 const rekomendasiContainer = document.getElementById("rekomendasi-container");
 
 // function render card
-function renderCard(data, container) {
-  data.forEach(item => {
-    const card = document.createElement("div");
-    card.classList.add("card");
+function renderCard(data, container){
 
-    card.innerHTML = `
-      <img src="${item.gambar}">
-    
-      <div class="card-body">
-    
-        <span class="badge">
-          ${item.kategori === "viral" ? "🔥 Viral" : "⭐ Rekomendasi"}
-        </span>
-    
-        <h3>${item.nama}</h3>
-    
-        <p>${item.deskripsi}</p>
-    
-        <button class="detailBtn">
-          Lihat Detail
-        </button>
-    
-      </div>
-    `;
+    container.innerHTML = "";
 
-    container.appendChild(card);
-  });
+    data.forEach(item=>{
+
+        const card = document.createElement("div");
+
+        card.className = "card";
+
+        card.innerHTML = `
+            <img src="${item.gambar}">
+
+            <div class="card-body">
+
+                <span class="badge">
+                    ${item.kategori === "viral" ? "🔥 Viral" : "⭐ Rekomendasi"}
+                </span>
+
+                <h3>${item.nama}</h3>
+
+                <p>${item.deskripsi}</p>
+
+                <button class="detailBtn">
+                    Lihat Detail
+                </button>
+
+            </div>
+        `;
+
+        container.appendChild(card);
+
+    });
+
 }
-
 // render
 renderCard(viral, viralContainer);
 renderCard(rekomendasi, rekomendasiContainer);
+
+function cariKuliner(){
+
+    const keyword =
+    document.getElementById("search").value.toLowerCase();
+
+    const hasilViral =
+    viral.filter(item =>
+        item.nama.toLowerCase().includes(keyword)
+    );
+
+    const hasilRekomendasi =
+    rekomendasi.filter(item =>
+        item.nama.toLowerCase().includes(keyword)
+    );
+
+    renderCard(hasilViral, viralContainer);
+    renderCard(hasilRekomendasi, rekomendasiContainer);
+
+}
