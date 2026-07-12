e95d67b497a5f93b9c6458f02325a8918563bee5
ellyzajuanaputri <ellyzajuanaputri@gmail.com>
2026-07-12 21:36:44 +0700

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://sdgpibwpqrhvkopxozmd.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_QJ13_u75jazZa6LGCaaAZA_85TjtQ16";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
