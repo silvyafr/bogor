e814d04ab613a39f3bfedd443cf386b696d9f95c
ellyzajuanaputri <ellyzajuanaputri@gmail.com>
2026-07-19 13:21:32 +0700

    Update database.js

--- database.js
+++ database.js
@@ -27,3 +27,47 @@ request.onerror = function () {
     console.log("IndexedDB gagal dibuat");
 
 };
+
+console.log("database.js berjalan");
+
+let dbLocal;
+
+const request = indexedDB.open("KulinerBogorDB", 1);
+
+request.onupgradeneeded = function (event) {
+
+    dbLocal = event.target.result;
+
+    dbLocal.createObjectStore("kuliner", {
+        keyPath: "id"
+    });
+
+};
+
+request.onsuccess = function (event) {
+
+    dbLocal = event.target.result;
+
+    console.log("IndexedDB berhasil dibuat");
+
+};
+
+request.onerror = function () {
+
+    console.log("IndexedDB gagal dibuat");
+
+};
+
+function simpanKeIndexedDB(data) {
+
+    const transaksi = dbLocal.transaction("kuliner", "readwrite");
+
+    const store = transaksi.objectStore("kuliner");
+
+    data.forEach(item => {
+        store.put(item);
+    });
+
+    console.log("Data berhasil disimpan ke IndexedDB");
+
+}
