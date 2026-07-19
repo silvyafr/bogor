aafdf1b7c5c5f11caa5d37ba776c930804d47d85
ellyzajuanaputri <ellyzajuanaputri@gmail.com>
2026-07-19 13:07:47 +0700

    Create database.js

---
+++ database.js
@@ -0,0 +1,27 @@
+let dbLocal;
+
+const request = indexedDB.open("KulinerBogorDB", 1);
+
+request.onupgradeneeded = function (event) {
+
+    dbLocal = event.target.result;
+
+    dbLocal.createObjectStore("kuliner", {
+        keyPath: "id"
+    });
+
+};
+
+request.onsuccess = function (event) {
+
+    dbLocal = event.target.result;
+
+    console.log("IndexedDB berhasil dibuat");
+
+};
+
+request.onerror = function () {
+
+    console.log("IndexedDB gagal dibuat");
+
+};
