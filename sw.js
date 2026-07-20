9e7ed22dbcc4310f80dbb9fdf282609b4262263e
silvyafr <afriliasilvy@gmail.com>
2026-07-20 23:34:24 +0700

    Update service worker cache name and URLs

--- sw.js
+++ sw.js
@@ -1,29 +1,106 @@
-const CACHE_NAME = "kuliner-bogor-v2";
+const CACHE_NAME = "bogor-pwa-v2";
 
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./database.js",
-  "./supabase.js",
-  "./manifest.json",
-  "./assets/icon-192.png",
-  "./assets/icon-512.png"
+
+    "/",
+    "index.html",
+
+    "makanan-khas.html",
+    "minuman-khas.html",
+    "minuman-viral.html",
+    "oleh-oleh.html",
+
+    "admin.html",
+    "admin-login.html",
+    "tambah.html",
+    "edit.html",
+
+    "style.css",
+    "cms.js",
+    "indexeddb.js",
+    "supabase.js",
+
+    "manifest.json",
+
+    "icon-192.png",
+    "icon-512.png"
+
 ];
 
+// Install
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+
+    );
+
+    self.skipWaiting();
+
 });
 
+// Activate
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys =>
+
+            Promise.all(
+
+                keys.map(key => {
+
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+
+                })
+
+            )
+
+        )
+
+    );
+
+    self.clients.claim();
+
+});
+
+// Fetch
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+
+    event.respondWith(
+
+        caches.match(event.request)
+
+            .then(response => {
+
+                return response || fetch(event.request)
+
+                    .then(networkResponse => {
+
+                        if (
+                            event.request.method === "GET" &&
+                            networkResponse.status === 200
+                        ) {
+
+                            const clone = networkResponse.clone();
+
+                            caches.open(CACHE_NAME)
+                                .then(cache => cache.put(event.request, clone));
+
+                        }
+
+                        return networkResponse;
+
+                    })
+
+                    .catch(() => caches.match("index.html"));
+
+            })
+
+    );
+
 });
